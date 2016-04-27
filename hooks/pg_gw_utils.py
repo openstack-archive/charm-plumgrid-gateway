721c1d27ba14b442de82f09636f0c34456cf07d1
Junaid Ali <junaidali@plumgrid.com>
2016-04-27 03:47:45 -0700

    Updated restart_pg function

--- hooks/pg_gw_utils.py
+++ hooks/pg_gw_utils.py
@@ -158,7 +158,9 @@ def restart_pg():
             if service_start('libvirt-bin'):
                 time.sleep(3)
                 if not service_running('plumgrid'):
-                    raise ValueError("plumgrid service couldn't be started")
+                    if not service_start('plumgrid'):
+                        raise ValueError("plumgrid service couldn't be started"
+                                         )
             else:
                 raise ValueError("libvirt-bin service couldn't be started")
     status_set('active', 'Unit is ready')
