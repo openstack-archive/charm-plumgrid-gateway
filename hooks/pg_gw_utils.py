86037a00c80417be68860620d1beef3314a4f549
Junaid Ali <junaidali@plumgrid.com>
2016-04-30 22:22:48 -0400

    update sleep time in restart_pg, changes for make sync

--- hooks/pg_gw_utils.py
+++ hooks/pg_gw_utils.py
@@ -156,7 +156,7 @@ def restart_pg():
             raise ValueError("plumgrid service couldn't be started")
         else:
             if service_start('libvirt-bin'):
-                time.sleep(5)
+                time.sleep(8)
                 if not service_running('plumgrid') \
                         and not service_start('plumgrid'):
                     raise ValueError("plumgrid service couldn't be started")
